21203d03375d7b96e23957f8673219b8a8189b31
pi <pi@devidiot.iptime.org>
2018-05-04 16:04:44 +0900

    version 0.1

--- src/pages/home/home.ts
+++ src/pages/home/home.ts
@@ -1,5 +1,5 @@
 import { Component } from '@angular/core';
-import { NavController } from 'ionic-angular';
+import { NavController, AlertController } from 'ionic-angular';
 
 @Component({
   selector: 'page-home',
@@ -7,8 +7,20 @@ import { NavController } from 'ionic-angular';
 })
 export class HomePage {
 
-  constructor(public navCtrl: NavController) {
+  constructor(public navCtrl: NavController, 
+    private alertCtrl: AlertController) {
 
+  }
+
+  sayHello() {
+    let alert = this.alertCtrl.create({
+      title: 'Greeting',
+      message: 'Hello World',
+      buttons: [
+        {text: 'OK'}
+      ]
+    });
+    alert.present();
   }
 
 }
